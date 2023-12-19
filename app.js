4528c18ce1c5c46b083b960e83aabe87f1c316ef
Shivam Pandey <shivampandey4194@gmail.com>
2023-12-19 22:59:43 +0530

    fixed cors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectDB = require('./utils/db');
 const authRoutes = require('./routes/authRoutes');
-const jwtMiddleware = require('./middleware/jwtMiddleware')
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
